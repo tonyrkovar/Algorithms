7a0a92e76566cb43d74de6b8da0b7b847a6bfa50
Tony <tonyrkovar@gmail.com>
2020-02-19 19:49:10 -0600

    feat: Started eating cookies finished stock prices

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,32 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n, a=1, cache=None,):
+  ways = 1
+  if n == 0:
+    return 1
+
+  combinations = 0
+
+  for i in range(n):
+    ways += 1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # for k in range(i):
+    #   combinations += 1
+    #   print(f"K: {k}, combinations: {combinations}")
+
+
+
+  return eating_cookies(n-1)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -14,3 +38,7 @@ if __name__ == "__main__":
     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
     print('Usage: eating_cookies.py [num_cookies]')
+   
+
+
+eating_cookies(4)
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,20 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  min_value = prices[0]
+  max_value = prices[len(prices) - 1]
+  
+  min_index = 0
+  max_index = len(prices) - 1
+  for i in range(1, len(prices)):
+    if prices[i] < min_value and max_index > i:
+      min_index = i
+      min_value = prices[i]
+    elif prices[i] > max_value and min_index < i:
+      max_index = i
+      max_value = prices[i]
+
+  return max_value - min_value
 
 
 if __name__ == '__main__':
@@ -13,3 +26,4 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
